4cb71168b78877cd98048a689d896c6faa749005
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-02 12:31:32 +0530

    Updated Updated

--- page/owner/update.php
+++ page/owner/update.php
@@ -13,42 +13,24 @@ class page_xAi_page_owner_update extends page_componentBase_page_update {
 		$this->update($dynamic_model_update=false); // All modls will be dynamic executed in here
 		
 		// Code to run after update
-
-		$model=$this->add('xAi/Model_Config');
-		$model->add('dynamic_model/Controller_AutoCreator');
-		$model->tryLoadAny();
-
-		$model=$this->add('xAi/Model_Dimension');
-		$model->add('dynamic_model/Controller_AutoCreator');
-		$model->tryLoadAny();
-
-		$model=$this->add('xAi/Model_IBlockContent');
-		$model->add('dynamic_model/Controller_AutoCreator');
-		$model->tryLoadAny();
-
-		$model=$this->add('xAi/Model_Session');
-		$model->add('dynamic_model/Controller_AutoCreator');
-		$model->tryLoadAny();
-
-		$model=$this->add('xAi/Model_MetaData');
-		$model->add('dynamic_model/Controller_AutoCreator');
-		$model->tryLoadAny();
-
-		$model=$this->add('xAi/Model_Data');
-		$model->add('dynamic_model/Controller_AutoCreator');
-		$model->tryLoadAny();
-
-		$model=$this->add('xAi/Model_Information');
-		$model->add('dynamic_model/Controller_AutoCreator');
-		$model->tryLoadAny();
-		
-		$model=$this->add('xAi/Model_InformationExtractor');
-		$model->add('dynamic_model/Controller_AutoCreator');
-		$model->tryLoadAny();		
-
-		$model=$this->add('xAi/Model_SalesExecutive');
-		$model->add('dynamic_model/Controller_AutoCreator');
-		$model->tryLoadAny();		
-
+		$model_array=array('Model_Config',
+							'Model_Dimension',
+							'Model_IBlockContent',
+							'Model_Session',
+							'Model_MetaData',
+							'Model_Data',
+							'Model_Information',
+							'Model_InformationExtractor',
+							'Model_SalesExecutive',
+							'Model_MetaInformation',
+							'Model_VisualAnalytic',
+							'Model_VisualAnalyticSeries'
+			);
+		foreach ($model_array as  $md) {
+			$model=$this->add('xAi/'.$md);
+			$model->add('dynamic_model/Controller_AutoCreator');
+			$model->tryLoadAny();
+		}
+		$this->add('View_Info')->set('Component Updated Successfully');
 	}
 }
