9763236a43add56cf7a0938699cdedefeb21256f
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-02 14:09:27 +0530

    Data Frequency removed from config

--- lib/Model/Config.php
+++ lib/Model/Config.php
@@ -16,7 +16,7 @@ class Model_Config extends \Model_Table{
 			->defaultValue(true)->caption('Activate Artificial Intelligence');
 
 		$this->addField('keep_site_constant_for_session')->type('boolean')->defaultValue(true);
-		$this->addField('send_data_frequency')->type('int')->defaultValue(6)->hint('Set 0 for immediate or no of seconds to send commulative tracking data')->system(true);
+		// $this->addField('send_data_frequency')->type('int')->defaultValue(6)->hint('Set 0 for immediate or no of seconds to send commulative tracking data')->system(true);
 
 		// $this->add('dynamic_model/Controller_AutoCreator');
 
